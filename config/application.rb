2510ffaeb9042a1bdb6c3c0a2702f707d958ee3c
Alexandre Tavares <xptavares@gmail.com>
2021-10-22 20:41:53 -0300

    configure the sidekiq to works in the project

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require "sidekiq"
+require "sidekiq/web"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +21,7 @@ module BlueCoadingParingSession
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.autoload_paths += [config.root.join("app")]
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
